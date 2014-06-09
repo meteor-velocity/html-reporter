2d2bcf2b4c5726079e068673147e5ff2ec0bfb13
Robert Dickert <robert.dickert@gmail.com>
2014-06-09 17:38:15 -0600

    Add bootstrap & scope to overlay only

--- package.js
+++ package.js
@@ -3,8 +3,13 @@ Package.describe({
 })
 
 Package.on_use(function(api) {
-  api.use(['velocity', 'templating','amplify'], "client");
+  api.use(['velocity', 'templating','amplify', 'less'], "client");
 
   api.add_files('lib/client-report.html', 'client');
   api.add_files('lib/client-report.js', 'client');
+  api.add_files('lib/client-report.less', 'client');
+  api.add_files('lib/bootstrap.less', 'client');
+  api.add_files('lib/bootstrap/fonts/glyphicons-halflings-regular.eot', 'client');
+  api.add_files('lib/bootstrap/fonts/glyphicons-halflings-regular.ttf', 'client');
+  api.add_files('lib/bootstrap/fonts/glyphicons-halflings-regular.woff', 'client');
 });
