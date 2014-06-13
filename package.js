e603b670263b8803c8f712762257f72575b3febf
Robert Dickert <robert.dickert@gmail.com>
2014-06-13 17:07:32 -0600

    move styles to files

--- package.js
+++ package.js
@@ -8,6 +8,7 @@ Package.on_use(function(api) {
   api.add_files('lib/client-report.html', 'client');
   api.add_files('lib/client-report.js', 'client');
   api.add_files('lib/client-report.less', 'client');
+  api.add_files('lib/status-widget.less', 'client');
 
   //bootstrap glyphicons
   api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.eot', 'client');
