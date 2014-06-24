755ee5811b5746f830e9bdbfd6236382e174e828
Robert Dickert <robert.dickert@gmail.com>
2014-06-24 09:51:49 -0600

    create reamplify: make amplify reactive

--- package.js
+++ package.js
@@ -4,6 +4,8 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.use(['velocity', 'templating','amplify', 'less'], "client");
+  
+  api.add_files('lib/reamplify.js', 'client');
 
   api.add_files('lib/client-report.html', 'client');
   api.add_files('lib/client-report.js', 'client');
@@ -15,4 +17,6 @@ Package.on_use(function(api) {
   api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.ttf', 'client');
   api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.woff', 'client');
   api.add_files('lib/bootstrap/src/fonts/glyphicons-halflings-regular.svg', 'client');
+
+  // api.export('reamplify', ['client']);
 });
