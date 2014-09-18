c469df0a8877782247f6a3f09ab362fcb9536579
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-09-18 19:59:32 +0200

    Fix asset paths for Meteor 0.9.2.
    This means that they are now broken in versions prior to Meteor 0.9.2.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "velocity:html-reporter",
   summary: "Reactive Velocity test reports in your app.",
-  version: "0.2.0",
+  version: "0.2.1",
   git: "https://github.com/rdickert/velocity-html-reporter.git"
 });
 
