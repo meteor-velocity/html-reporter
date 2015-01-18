74f081ab986309e7f575c2063c1bdf2eddd27a55
sam@hatoum.net <sam@hatoum.net>
2015-01-18 13:18:00 -0800

    bumping versions

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'velocity:html-reporter',
   summary: 'Reactive Velocity test reports in your app.',
-  version: '0.3.2',
-  git: 'https://github.com/rdickert/velocity-html-reporter.git',
+  version: '0.4.1',
+  git: 'https://github.com/meteor-velocity/html-reporter.git',
   debugOnly: true
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
-  api.use('velocity:core@0.3.2', 'client');
+  api.use('velocity:core@0.4.5', 'client');
 
   api.use(['underscore', 'templating','amplify@1.0.0', 'less'], 'client');
 
