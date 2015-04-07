59edfce7c5f695642f9030840784088d6380c78d
sam@hatoum.net <sam@hatoum.net>
2015-04-07 13:20:19 -0700

    should be 0.5.0 since this is a minor increment

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'velocity:html-reporter',
   summary: 'Reactive Velocity test reports in your app.',
-  version: '0.5.2',
+  version: '0.5.0',
   git: 'https://github.com/meteor-velocity/html-reporter.git',
   debugOnly: true
 });
