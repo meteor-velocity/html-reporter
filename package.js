0eeeac2b8770af5838ecfda779fae0a47e42a52c
sam@hatoum.net <sam@hatoum.net>
2015-05-16 20:23:50 -0700

    bumping release verison

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'velocity:html-reporter',
   summary: 'Reactive Velocity test reports in your app.',
-  version: '0.6.0',
+  version: '0.6.1',
   git: 'https://github.com/meteor-velocity/html-reporter.git',
   debugOnly: true
 });
