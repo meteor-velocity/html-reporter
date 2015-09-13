cbb65b5a2b33f7b3f633bf46a42ce262c20fd721
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-13 15:27:28 +0200

    Make package compatible with Meteor 1.2

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'velocity:html-reporter',
   summary: 'Reactive Velocity test reports in your app.',
-  version: '0.8.2',
+  version: '0.9.0-rc.2',
   git: 'https://github.com/meteor-velocity/html-reporter.git',
   debugOnly: true
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.1.0.2');
+  api.versionsFrom('METEOR@1.2-rc.14');
   api.use('grigio:babel@0.1.6');
   api.use('velocity:core@0.7.1', 'client');
 
@@ -18,10 +18,11 @@ Package.onUse(function(api) {
   api.addFiles('lib/velocity.es6.js', 'client');
   api.addFiles('lib/client-report.html', 'client');
   api.addFiles('lib/client-report.es6.js', 'client');
+  api.addFiles('lib/velocity.import.less', 'client');
   api.addFiles('lib/client-report.less', 'client');
   api.addFiles('lib/status-widget.less', 'client');
 
-  api.addFiles('lib/velocity-logo.png');
-  api.addFiles('lib/velocity_cog.svg');
-  api.addFiles('lib/icon-time.png');
+  api.addAssets('lib/velocity-logo.png', 'client');
+  api.addAssets('lib/velocity_cog.svg', 'client');
+  api.addAssets('lib/icon-time.png', 'client');
 });
