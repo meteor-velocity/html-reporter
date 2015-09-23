04bb5c652a7aa66999654771cc675b6ef428360b
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-23 13:02:08 +0200

    Use velocity:core@0.10.0

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.2.0.1');
   api.use('ecmascript');
-  api.use('velocity:core@0.10.0-rc.4', 'client');
+  api.use('velocity:core@0.10.0', 'client');
 
   api.use([
     'underscore',
